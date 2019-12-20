eacf5a7afec4705d12350ea2c8704eb759910e9f
Tapiwa Pitso <tapiwapitso@gmail.com>
2019-12-20 23:56:01 +0200

    Added the ability for groom to add custom message

--- app/src/main/java/android/cloudpoint/com/tedwedskids/MainActivity.java
+++ app/src/main/java/android/cloudpoint/com/tedwedskids/MainActivity.java
@@ -10,8 +10,10 @@ import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -23,7 +25,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
     private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 0;
-    Button sendMessages;
+    Button sendMessages, customSmsBtn;
+    EditText customSmsTxt;
     final String phoneNo = "+26774319137";
     final String textMessage = "Thank You for celebrating our wedding with us";
 
@@ -41,6 +44,9 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+        // initialise objects
+        customSmsTxt = findViewById(R.id.customSmsTxt);
+        customSmsBtn = findViewById(R.id.customSmsBtn);
         sendMessages = findViewById(R.id.sendMessages);
 
         Retrofit retrofit = new Retrofit.Builder().baseUrl(Api.baseUrl).addConverterFactory(GsonConverterFactory.create()).build();
@@ -49,15 +55,50 @@ public class MainActivity extends AppCompatActivity {
 
         final Call<List<Guest>> call = api.getGuests();
 
+        // Sending the Hard coded Thank you Messages
         sendMessages.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 queueCall(call);
             }
+        }); // sendMessages
+
+        customSmsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = customSmsTxt.getText().toString();
+                sendCustomMessage(call, text);
+                customSmsTxt.setText("");
+                Toast.makeText(getApplicationContext(), "Sent!", Toast.LENGTH_LONG).show();
+            }
         });
 
-
     } //onCreate
+
+    private void sendCustomMessage(Call<List<Guest>> call, final String text) {
+        call.enqueue(new Callback<List<Guest>>() {
+            @Override
+            public void onResponse(Call<List<Guest>> call, Response<List<Guest>> response) {
+                List<Guest> guests = response.body();
+
+                for (Guest guest : guests) {
+                    if (guest.getName() != null && guest.getSurname() != null && guest.getNumber() != null) {
+                        Log.d("Debugging: ", "Getting Data From the API");
+
+                        String message = "Hi, " + guest.getName() + "." + text;
+                        sendTextMessage(message, guest.getNumber());
+                        Toast.makeText(getApplicationContext(), "Sending...", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Guest>> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
+                System.out.println(".............." + t.getMessage());
+            }
+        });
+    }
 
     private void queueCall(Call<List<Guest>> call) {
         call.enqueue(new Callback<List<Guest>>() {
@@ -67,9 +108,7 @@ public class MainActivity extends AppCompatActivity {
 
                 for (Guest guest : guests) {
                     if (guest.getName() != null && guest.getSurname() != null && guest.getNumber() != null) {
-                        Log.d("Name: ", guest.getName());
-                        Log.d("Surname: ", guest.getSurname());
-                        Log.d("Number: ", guest.getNumber());
+                        Log.d("Debugging: ", "Getting Data From the API");
 
                         createMessage(guest);
                     } else {
